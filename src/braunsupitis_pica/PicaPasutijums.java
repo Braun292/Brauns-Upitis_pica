e39432981a0633ed9c3f48557a21add226964dce
Braun292 <danielsbrauns61@gmail.com>
2025-04-15 04:59:03 +0200

    Pievienota iespēja izvēlēties picu veidu un izmēru.

--- src/braunsupitis_pica/PicaPasutijums.java
+++ src/braunsupitis_pica/PicaPasutijums.java
@@ -7,7 +7,16 @@ public class PicaPasutijums {
         JOptionPane.showMessageDialog(null, "Sveicināti mūsu picērijā!", "Picērija", JOptionPane.INFORMATION_MESSAGE);
         
         String vards = ievadiTekstu("vārdu", "[a-zA-Z]+", "Tikai burti!");
+
+        String[] picuVeidi = {"Siera pica", "Peperoni pica", "Studentu pica"};
+        int veids = JOptionPane.showOptionDialog(null, "Izvēlieties picu:", "Pasūtījums",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, picuVeidi, picuVeidi[0]);
+
+        String[] izmeri = {"Maza (25cm)", "Vidēja (30cm)", "Liela (35cm)"};
+        String izmers = (String) JOptionPane.showInputDialog(null, "Izvēlieties izmēru:", "Izmērs",
+                JOptionPane.QUESTION_MESSAGE, null, izmeri, izmeri[0]);
     }
+
     private static String ievadiTekstu(String lauks, String regex, String kluda) {
         while (true) {
             String ievade = JOptionPane.showInputDialog("Ievadiet savu " + lauks + ":");
