237266565871ad34e92bb459d7cb7e7c89b92f34
Braun292 <danielsbrauns61@gmail.com>
2025-04-15 04:09:58 +0200

    pievienoju paligfunkciju tekstu ievadei ar validaciju

--- src/braunsupitis_pica/PicaPasutijums.java
+++ src/braunsupitis_pica/PicaPasutijums.java
@@ -1,8 +1,18 @@
 package braunsupitis_pica;
+
 import javax.swing.*;
 
 public class PicaPasutijums {
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null, "Sveicināti mūsu picērijā!", "Picērija", JOptionPane.INFORMATION_MESSAGE);
+        
+        String vards = ievadiTekstu("vārdu", "[a-zA-Z]+", "Tikai burti!");
+    }
+    private static String ievadiTekstu(String lauks, String regex, String kluda) {
+        while (true) {
+            String ievade = JOptionPane.showInputDialog("Ievadiet savu " + lauks + ":");
+            if (ievade != null && ievade.matches(regex)) return ievade;
+            JOptionPane.showMessageDialog(null, kluda, "Kļūda", JOptionPane.ERROR_MESSAGE);
+        }
     }
 }
