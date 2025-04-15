4e5ca4f989706d3213c8285ce7c60f05e9e8f9e7
Braun292 <danielsbrauns61@gmail.com>
2025-04-15 03:44:45 +0200

    pievienots sveicina logs

--- src/braunsupitis_pica/PicaPasutijums.java
+++ src/braunsupitis_pica/PicaPasutijums.java
@@ -1,5 +1,8 @@
 package braunsupitis_pica;
+import javax.swing.*;
 
 public class PicaPasutijums {
-
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Sveicināti mūsu picērijā!", "Picērija", JOptionPane.INFORMATION_MESSAGE);
+    }
 }
