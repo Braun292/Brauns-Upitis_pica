a9ab47ee7b6c6b420b8dbdb6588faf881ba181c3
Braun292 <danielsbrauns61@gmail.com>
2025-04-15 04:32:42 +0200

    Izveidota Pica klase ar privātiem mainīgajiem (vards, talrunis, adrese, izmers, piegade, cena) un konstruktoru

--- src/braunsupitis_pica/Pica.java
+++ src/braunsupitis_pica/Pica.java
@@ -1,5 +1,18 @@
 package braunsupitis_pica;
 
 public class Pica {
+    private String vards;
+    private String talrunis;
+    private String adrese;
+    private String izmers;
+    private boolean piegade;
+    private double cena;
 
+    public Pica(String vards, String talrunis, String adrese, String izmers, boolean piegade) {
+        this.vards = vards;
+        this.talrunis = talrunis;
+        this.adrese = adrese;
+        this.izmers = izmers;
+        this.piegade = piegade;
+    }
 }
