54119a10bffc57ad83e855385c6a43c676db63d3
Braun292 <danielsbrauns61@gmail.com>
2025-04-15 04:43:13 +0200

    Pievienota metode aprekinatCenu(), kas aprēķina picu cenu atkarībā no izvēlētā izmēra un piegādes. Un pievienota metode toFileString(), kas pārveido Pica objektu par tekstu, kuru var saglabāt failā.

--- src/braunsupitis_pica/Pica.java
+++ src/braunsupitis_pica/Pica.java
@@ -15,4 +15,28 @@ public class Pica {
         this.izmers = izmers;
         this.piegade = piegade;
     }
+
+    private double aprekinatCenu() {
+        double cena = 0;
+        switch (izmers) {
+            case "Maza":
+                cena += 5.0; 
+                break;
+            case "Videja":
+                cena += 7.0;
+                break;
+            case "Liela":
+                cena += 10.0;
+                break;
+        }
+
+        if (piegade) {
+            cena += 2.0;
+        }
+        return cena;
+    }
+
+    public String toFileString() {
+        return vards + "," + talrunis + "," + adrese + "," + izmers + "," + piegade + "," + cena;
+    }
 }
